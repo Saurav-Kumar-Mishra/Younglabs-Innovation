88b1c6041cadb6fc28df8af1a0c04b2e1ce26b5e
Saurav-Kumar-Mishra <sauravkumarmishra600@gmail.com>
2025-03-07 13:53:06 +0530

    final committed

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+const errorMsg = {                       // error object
+    error: 'Name is required.',
+}
+
+app.get('/api/greet', (request, response) => {
+    const name = request.query.name
+   
+    if (!name || name.trim() === "") {
+        response.status(400).json(errorMsg) // Bad request when 'name' is missing
+    } else {
+        response
+            .status(200)
+            .json({ message: `Hello, ${name}! Welcome to Younglabs.` }) // Success response
+            
+    }
+    
+})
+
+app.listen(process.env.PORT || 3002, () => {
+    console.log(`Server started on port ${process.env.PORT}`)
+})

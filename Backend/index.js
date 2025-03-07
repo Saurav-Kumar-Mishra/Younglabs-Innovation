043c7d33558e82b2ff692c721935c23c19db0e69
Saurav-Kumar-Mishra <sauravkumarmishra600@gmail.com>
2025-03-07 14:05:52 +0530

    fix default route

--- Backend/index.js
+++ Backend/index.js
@@ -6,22 +6,24 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
-const errorMsg = {                       // error object
+const errorMsg = {
+    // error object
     error: 'Name is required.',
 }
+app.get('/', (request, response) => {
+    response.status(200).send('Api is active')
+})
 
 app.get('/api/greet', (request, response) => {
     const name = request.query.name
-   
-    if (!name || name.trim() === "") {
+
+    if (!name || name.trim() === '') {
         response.status(400).json(errorMsg) // Bad request when 'name' is missing
     } else {
         response
             .status(200)
             .json({ message: `Hello, ${name}! Welcome to Younglabs.` }) // Success response
-            
     }
-    
 })
 
 app.listen(process.env.PORT || 3002, () => {
